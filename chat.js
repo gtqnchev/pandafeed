2fca39b4d9e5bd478a0625da6041a8cfbfd8e3e0
George <gtqnchev@gmail.com>
2015-02-12 11:38:51 +0200

    Components scaffold

--- chat.js
+++ chat.js
@@ -1,7 +1,6 @@
 var React = require('react');
 var socket = io();
 
-
 socket.on('welcome', function (data) {
     console.log('welcome: ', data);
 });
@@ -14,7 +13,84 @@ socket.on('message', function(data) {
     console.log('message: ', data);
 });
 
+var MessageBox = React.createClass({
+    render: function() {
+        return (
+            <div className="messageBox">
+                <h1>Messages</h1>
+                <MessageList />
+                <MessageForm />
+            </div>
+        );
+    }
+});
+
+var MessageList = React.createClass({
+    render: function() {
+        return (
+            <div className="messageList">
+            messageList
+            </div>
+        );
+    }
+});
+
+var Message = React.createClass({
+    render: function() {
+        return (
+            <div className="message">
+            message
+            </div>
+        );
+    }
+});
+
+var MessageForm = React.createClass({
+    render: function() {
+        return (
+            <div className="messageForm">
+            messageForm
+            </div>
+        );
+    }
+});
+
+
+var UserBox = React.createClass({
+    render: function() {
+        return (
+            <div className="userBox">
+                <h1>Users</h1>
+                <UserList />
+            </div>
+        );
+    }
+});
+
+var UserList = React.createClass({
+    render: function() {
+        return (
+            <div className="userList">
+            userList
+            </div>
+        );
+    }
+});
+
+var User = React.createClass({
+    render: function() {
+        return (
+            <div className="user">
+                user
+            </div>
+        );
+    }
+});
+
 React.render(
-    <h1>Reactive world, OMG!</h1>,
+    <div>
+        <MessageBox />
+        <UserBox />
+    </div>,
     document.getElementById('react')
 );
