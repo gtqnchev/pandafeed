1c927420f9fb5a81abbd7ecd6db0b08595d57168
George <gtqnchev@gmail.com>
2015-02-13 10:14:14 +0200

    Change database name

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost:27017/pandafeed');
 var Schema = mongoose.Schema;
 var crypto = require('crypto');
 
@@ -7,9 +7,9 @@ var userSchema = new Schema({ name: String, password: String, token: String });
 var User = mongoose.model('User', userSchema);
 
 User.prototype.generateToken = function() {
-	var date = (new Date()).valueOf().toString(),
-		number = Math.random().toString();
-	this.token = crypto.createHash('md5').update(date + number).digest('hex');
-}
+    var date = (new Date()).valueOf().toString(),
+        number = Math.random().toString();
+    this.token = crypto.createHash('md5').update(date + number).digest('hex');
+};
 
 module.exports = User;
